08107833ea8d0d1cc05690195f7145c33bb17d3c
Callum <51374718+aarctical@users.noreply.github.com>
2022-02-20 14:29:18 +0000

    Updated to latest Andy Voice Range client
    Added betterHUD, compatible with all andy features of the script

--- client.lua
+++ client.lua
@@ -1,146 +1,146 @@
--- Config
-local Config = {
-	x = 0.165,
-	y = 0.95,
+local config = {
+    x = 0.165,
+    y = 0.95,
     scale = 0.40,
 
-    WhisperDistance = 2.0,
-    NormalDistance = 10.0,
-    ShoutingDistance = 30.0,
-    
-    Keybind = 20
+    x2 = 0.157,
+    y2 = 0.965,
+
+    Keybind = 20, -- https://docs.fivem.net/docs/game-references/controls/
+    customKeybind = true, -- if you enable this then players can bind the key through their settings and not the Keybind above.
+
+    enableBlueCircle = false, -- this will enable the blue circle to show you the distance you're voice can reach.
+    makeHudSmallerWhileSpeaking = false, -- alternative to betterHUD (below), makes hud smaller when PTT activated
+
+    betterHUD = true, -- W/N/S display
+    isPlayerSpeakingCheck = true, -- do not disable
+
+    changeSpeakingDistance = true, -- This will change your voice distance based on what you've chosen. This is not recommended to be off unless you also want to turn off hearing and use this whole script only as a hud.
+    changeHearingDistance = false, -- This will change your hearing distance based on what you've chosen. This is recommended to be off.
+
+    ranges = {
+        {distance = 2.5, name = "🔈"},
+        {distance = 5.0, name = "🔉"},
+        {distance = 15.0, name = "🔊"}
+    }
 }
 
-local Config2 = {
-	x2 = 0.157,
-	y2 = 0.97,
-    scale = 0.40,
-}
-
-local InputWhisper = false
-local InputNormal = true
-local InputShouting = false
+-- Don't change anything below the config unless you know what you're doing. For support join the discord: https://discord.gg/eKFb5QM3YF
+local keybindUsed = false
 local isTalking = false
-local CurrentDistance = Config.NormalDistance
+local CurrentChosenDistance = 2
+local CurrentDistanceValue = config.ranges[CurrentChosenDistance].distance
+local CurrentDistanceName = config.ranges[CurrentChosenDistance].name
 
-function InputText(text, scale)
+function text(text, scale)
     SetTextFont(4)
     SetTextProportional(0)
     SetTextScale(scale, scale)
     SetTextEdge(1, 0, 0, 0, 255)
     SetTextDropShadow(0, 0, 0, 0,255)
     SetTextOutline()
-	SetTextJustification(0)
+    SetTextJustification(0)
     SetTextEntry("STRING")
     AddTextComponentString(text)
-    DrawText(Config.x, Config.y)
+    DrawText(config.x, config.y)
+end
+
+if config.customKeybind then
+    RegisterCommand("+voiceDistance", function()
+        keybindUsed = true
+        keybindUsed = false
+    end, false)
+    RegisterCommand("-voiceDistance", function()
+        keybindUsed = false
+    end, false)
+    RegisterKeyMapping("+voiceDistance", "Change Voice Proximity", "keyboard", "z")
+end
+
+-- Check if player is speaking
+if config.isPlayerSpeakingCheck then
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(50)
+            if NetworkIsPlayerTalking(PlayerId()) then
+                isTalking = true
+            else
+                isTalking = false
+            end
+        end
+    end)
 end
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        if IsControlJustPressed(0, Config.Keybind) then
-            if InputWhisper == true then
-                MumbleSetAudioInputDistance(Config.NormalDistance)
-                -- MumbleSetAudioOutputDistance(Config.NormalDistance)
-                CurrentDistance = Config.NormalDistance
-                InputWhisper = false
-                InputNormal = true
-                InputShouting = false
-            elseif InputNormal == true then
-                MumbleSetAudioInputDistance(Config.ShoutingDistance)
-                -- MumbleSetAudioOutputDistance(Config.ShoutingDistance)
-                CurrentDistance = Config.ShoutingDistance
-                InputWhisper = false
-                InputNormal = false
-                InputShouting = true
-            elseif InputShouting == true then
-                MumbleSetAudioInputDistance(Config.WhisperDistance)
-                -- MumbleSetAudioOutputDistance(Config.WhisperDistance)
-                CurrentDistance = Config.WhisperDistance
-                InputWhisper = true
-                InputNormal = false
-                InputShouting = false
+        -- change voice
+        if IsControlJustPressed(0, config.Keybind) or keybindUsed then
+            if CurrentChosenDistance == #config.ranges then
+                CurrentChosenDistance = 1
+            else
+                CurrentChosenDistance = CurrentChosenDistance + 1
+            end
+            CurrentDistanceValue = config.ranges[CurrentChosenDistance].distance
+            CurrentDistanceName = config.ranges[CurrentChosenDistance].name
+            if config.changeSpeakingDistance then
+                MumbleSetAudioInputDistance(CurrentDistanceValue)
+            end
+            if config.changeHearingDistance then
+                MumbleSetAudioOutputDistance(CurrentDistanceValue)
             end
         end
-        if IsControlPressed(1, Config.Keybind) then
-            local pedCoords = GetEntityCoords(PlayerPedId())
-            --DrawMarker(1, pedCoords.x, pedCoords.y, pedCoords.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, CurrentDistance * 2.0, CurrentDistance * 2.0, 1.0, 40, 140, 255, 150, false, false, 2, false, nil, nil, false)
-        end
-    end
-end)
 
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(50)
-
-        if isTalking == false then
-            if NetworkIsPlayerTalking(PlayerId()) then
-                isTalking = true
+        -- Blue circle
+        if config.enableBlueCircle then
+            if IsControlPressed(1, config.Keybind) or keybindUsed then
+                local pedCoords = GetEntityCoords(PlayerPedId())
+                DrawMarker(1, pedCoords.x, pedCoords.y, pedCoords.z - 1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, CurrentDistanceValue * 2.0, CurrentDistanceValue * 2.0, 1.0, 40, 140, 255, 150, false, false, 2, false, nil, nil, false)
             end
+        end
+
+        -- HUD
+        if config.makeHudSmallerWhileSpeaking and isTalking then
+            text(CurrentDistanceName, config.scale / 1.25)
         else
-            if NetworkIsPlayerTalking(PlayerId()) == false then
-                isTalking = false
+            text(CurrentDistanceName, config.scale)
+        end
+
+        -- better hud
+        if config.betterHUD then
+            SetTextScale(0.0, 0.3)
+            SetTextFont(6)
+            SetTextProportional(1)
+            SetTextDropshadow(0, 0, 0, 0, 255)
+            SetTextEdge(1, 0, 0, 0, 255)
+            SetTextDropShadow()
+            SetTextOutline()
+            if CurrentChosenDistance == 1 and isTalking == false then
+                SetTextEntry("STRING")
+                AddTextComponentString("W")
+                DrawText(config.x2, config.y2)
+            elseif CurrentChosenDistance == 2 and isTalking == false then
+                SetTextEntry("STRING")
+                AddTextComponentString("N")
+                DrawText(config.x2, config.y2)
+            elseif CurrentChosenDistance == 3 and isTalking == false then
+                SetTextEntry("STRING")
+                AddTextComponentString("S")
+                DrawText(config.x2, config.y2)
+            elseif CurrentChosenDistance == 1 and isTalking == true then
+                SetTextEntry("STRING")
+                AddTextComponentString("~b~W")
+                DrawText(config.x2, config.y2)
+            elseif CurrentChosenDistance == 2 and isTalking == true then
+                SetTextEntry("STRING")
+                AddTextComponentString("~b~N")
+                DrawText(config.x2, config.y2)
+            elseif CurrentChosenDistance == 3 and isTalking == true then
+                SetTextEntry("STRING")
+                AddTextComponentString("~b~S")
+                DrawText(config.x2, config.y2)
             end
         end
-	end
+    end
 end)
--- icon hud
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
 
-        if InputWhisper == true and isTalking == false then
-            InputText("🔈", Config.scale)
-        elseif InputNormal == true and isTalking == false then
-            InputText("🔉", Config.scale)
-        elseif InputShouting == true and isTalking == false then
-            InputText("🔊", Config.scale)
-        elseif InputWhisper == true and isTalking == true then
-            InputText("🔈", Config.scale)
-        elseif InputNormal == true and isTalking == true then
-            InputText("🔉", Config.scale)
-        elseif InputShouting == true and isTalking == true then
-            InputText("🔊", Config.scale)
-        end
-    end
-end)
--- W/N/S hud
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        SetTextScale(0.0, 0.3)
-        SetTextFont(6)
-        SetTextProportional(1)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(1, 0, 0, 0, 255)
-        SetTextDropShadow()
-        SetTextOutline()
-        if InputWhisper == true and isTalking == false then
-            SetTextEntry("STRING")
-            AddTextComponentString("W")
-            DrawText(Config2.x2, Config2.y2)
-        elseif InputNormal == true and isTalking == false then
-            SetTextEntry("STRING")
-            AddTextComponentString("N")
-            DrawText(Config2.x2, Config2.y2)
-        elseif InputShouting == true and isTalking == false then
-            SetTextEntry("STRING")
-            AddTextComponentString("S")
-            DrawText(Config2.x2, Config2.y2)
-       elseif InputWhisper == true and isTalking == true then
-            SetTextEntry("STRING")
-            AddTextComponentString("~b~W")
-            DrawText(Config2.x2, Config2.y2)
-        elseif InputNormal == true and isTalking == true then
-            SetTextEntry("STRING")
-            AddTextComponentString("~b~N")
-            DrawText(Config2.x2, Config2.y2)
-        elseif InputShouting == true and isTalking == true then
-            SetTextEntry("STRING")
-            AddTextComponentString("~b~S")
-            DrawText(Config2.x2, Config2.y2)
-        end
-    end
-end)
